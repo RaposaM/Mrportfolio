c9aa6784e10ff08e4073df60c225c00680db0bcf
RaposaM <raposamitchell@gmail.com>
2026-05-01 10:01:50 -0700

    changed routing

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     Component: Layout,
